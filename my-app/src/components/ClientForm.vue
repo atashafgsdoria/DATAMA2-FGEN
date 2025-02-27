d4092829967fda0e30cad2e303c5c83d0501f1c0
Atasha Frances Gayle Doria <7mark.gayle@gmail.com>
2025-02-27 13:39:37 +0000

    .

--- my-app/src/components/ClientForm.vue
+++ my-app/src/components/ClientForm.vue
@@ -46,7 +46,10 @@
 import { createClient } from '@supabase/supabase-js';
 import { mapActions } from 'vuex';
 
-const supabase = createClient('YOUR_SUPABASE_URL', 'YOUR_SUPABASE_ANON_KEY');
+const SUPABASE_URL = 'https://qlldaarhspqfvvfpnuqf.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsbGRhYXJoc3BxZnZ2ZnBudXFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2NTY4ODIsImV4cCI6MjA1NjIzMjg4Mn0.Q3BCKZzUZ2oas4qPAj9qCNZekuYmSTDRPRcFrl7Wro4';
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default {
   data() {
